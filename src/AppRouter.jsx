31f67d9d3936d4954d34eb49e21b2974dad7a5cb
Your Name <you@example.com>
2024-02-24 01:58:33 -0500

    [ADD] Added @heroicons/react dependency and update AppRouter with navbar

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import { NewProductPage } from "./user/pages/NewProductPage/NewProductPage"
+import { NavBar } from "./components/NavBar/NavBar"
 
 export function AppRouter() {
   return (
     <Routes>
-        <Route path="/" element={<NewProductPage />} />
+        <Route path="/" element={<NavBar />} >
+          <Route index element={<NewProductPage />} />
+        </Route>
     </Routes>
   )
 }
---
+++ src/components/ProfileMenu/ProfileMenu.jsx
@@ -0,0 +1,86 @@
+import {
+  Typography,
+  Button,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+} from "@material-tailwind/react";
+
+import {
+  ChevronDownIcon,
+  PowerIcon,
+} from "@heroicons/react/24/solid";
+
+import { useState, createElement } from "react";
+
+const profileMenuItems = [
+  {
+    label: "Sign Out",
+    icon: PowerIcon,
+  },
+];
+
+export function ProfileMenu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const closeMenu = () => setIsMenuOpen(false);
+
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+      <MenuHandler>
+        <Button
+          variant="text"
+          color="blue-gray"
+          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+        >
+          <div className="flex items-center gap-4">
+        <Avatar src="https://docs.material-tailwind.com/img/face-2.jpg"  alt="avatar" />
+        <div>
+          <Typography variant="h6">Tania Andrew</Typography>
+          <Typography variant="small" color="gray" className="font-normal">
+            Admin
+          </Typography>
+        </div>
+      </div>
+          <ChevronDownIcon
+            strokeWidth={2.5}
+            className={`h-3 w-3 transition-transform ${
+              isMenuOpen ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+      <MenuList className="p-1">
+        {profileMenuItems.map(({ label, icon }, key) => {
+          const isLastItem = key === profileMenuItems.length - 1;
+          return (
+            <MenuItem
+              key={label}
+              onClick={closeMenu}
+              className={`flex items-center gap-2 rounded ${
+                isLastItem
+                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                  : ""
+              }`}
+            >
+              {createElement(icon, {
+                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                strokeWidth: 2,
+              })}
+              <Typography
+                as="span"
+                variant="small"
+                className="font-normal"
+                color={isLastItem ? "red" : "inherit"}
+              >
+                {label}
+              </Typography>
+            </MenuItem>
+          );
+        })}
+      </MenuList>
+    </Menu>
+  );
+}
