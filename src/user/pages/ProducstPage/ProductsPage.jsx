7413b971fcd378ed0291a67d271ea463dae5ff22
Your Name <you@example.com>
2024-02-25 13:55:06 -0500

    [UPDATE] Ended initial table without pagination

--- src/user/pages/ProducstPage/ProductsPage.jsx
+++ src/user/pages/ProducstPage/ProductsPage.jsx
@@ -1,7 +1,17 @@
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Switch, Tooltip} from "@nextui-org/react";
-import {EditIcon} from "./EditIcon";
-import {DeleteIcon} from "./DeleteIcon";
-import {EyeIcon} from "./EyeIcon";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Switch,
+  Tooltip,
+  Image,
+} from "@nextui-org/react";
+import { EditIcon } from "./EditIcon";
+import { DeleteIcon } from "./DeleteIcon";
+import { EyeIcon } from "./EyeIcon";
 import { columns, users } from "../../../data/data";
 import { useCallback } from "react";
 import { Header } from "../../../components/Header/Header";
@@ -11,27 +21,26 @@ export function ProductsPage() {
     const cellValue = user[columnKey];
 
     switch (columnKey) {
+      case "product":
+        return (
+          <div>
+            <Image width={35} alt="Image" src={user.product} />
+          </div>
+        );
       case "name":
         return (
-          <User
-            avatarProps={{radius: "lg", src: user.avatar}}
-            description={user.email}
-            name={cellValue}
-          >
-            {user.email}
-          </User>
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize font-poppins">{cellValue}</p>
+          </div>
         );
-      case "role":
+      case "price":
         return (
           <div className="flex flex-col">
-            <p className="text-bold text-sm capitalize">{cellValue}</p>
-            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+            <p className="text-bold text-sm capitalize font-poppins">$ {cellValue}</p>
           </div>
         );
       case "status":
-        return (
-          <Switch  />
-        );
+        return <Switch />;
       case "actions":
         return (
           <div className="relative flex items-center gap-2">
@@ -62,22 +71,26 @@ export function ProductsPage() {
       <Header pageName="Products" />
 
       <Table>
-      <TableHeader columns={columns}>
-        {(column) => (
-          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
-            {column.name}
-          </TableColumn>
-        )}
-      </TableHeader>
-      <TableBody items={users}>
-        {(item) => (
-          <TableRow key={item.id}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-      
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === "actions" ? "center" : "start"}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={users}>
+          {(item) => (
+            <TableRow key={item.id}>
+              {(columnKey) => (
+                <TableCell>{renderCell(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
     </main>
   );
 }
